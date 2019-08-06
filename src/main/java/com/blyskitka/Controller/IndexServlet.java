bd848e0478b5da419e2f624f0123dd2c1df09a71
marta <martavoloshyn>
2019-08-06 23:16:35 +0300

    delete unnecessary imports

--- src/main/java/com/blyskitka/Controller/IndexServlet.java
+++ src/main/java/com/blyskitka/Controller/IndexServlet.java
@@ -1,14 +1,10 @@
 package com.blyskitka.Controller;
 
 import com.blyskitka.Entity.Category;
-import com.blyskitka.Entity.Kind;
 import com.blyskitka.Service.Implementations.CategoryServiceImpl;
-import com.blyskitka.Service.Implementations.KindServiceImpl;
 import com.blyskitka.Service.Interfaces.CategoryService;
-import com.blyskitka.Service.Interfaces.KindService;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
--- src/main/java/com/blyskitka/Controller/SearchServlet.java
+++ src/main/java/com/blyskitka/Controller/SearchServlet.java
@@ -1,25 +1,15 @@
 package com.blyskitka.Controller;
 
 import com.blyskitka.Entity.Article;
-import com.blyskitka.Entity.Category;
-import com.blyskitka.Entity.Kind;
 import com.blyskitka.Service.Implementations.ArticleServiceImpl;
-import com.blyskitka.Service.Implementations.CategoryServiceImpl;
-import com.blyskitka.Service.Implementations.KindServiceImpl;
 import com.blyskitka.Service.Interfaces.ArticleService;
-import com.blyskitka.Service.Interfaces.CategoryService;
-import com.blyskitka.Service.Interfaces.KindService;
 import com.google.gson.Gson;
-import jdk.nashorn.internal.parser.JSONParser;
-
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
 public class SearchServlet extends HttpServlet {
